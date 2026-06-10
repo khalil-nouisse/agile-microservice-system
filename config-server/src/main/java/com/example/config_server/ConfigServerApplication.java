e4f9ca59fa2062c5bf798bbe80470a342f25dae6
lakhtiri-yassir <lakhtiri.yassir@gmail.com>
2026-06-10 20:41:52 +0100

    config server setup

--- config-server/src/main/java/com/example/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/config_server/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.agile.configserver;
+package com.example.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
